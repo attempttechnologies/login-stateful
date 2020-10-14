5c1bfad0b46f463d498c525b18cbfe24c52ffba0
Sulejman Limaj <limaj.sulejman@gmail.com>
2020-10-14 11:09:59 +0200

    lesson 107

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(),
+            TextFormField(),
+            RaisedButton(
+              onPressed: () {},
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
